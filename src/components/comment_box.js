12df084ef4eb9e522ef57d47c436fbc435e6b143
deepak6sp <deepak6sp@gmail.com>
2016-08-22 23:14:24 +0530

    comment box assertions tested

---
+++ src/components/comment_box.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class CommentBox extends Component {
+  render() {
+    return (
+      <div className="comment-box">
+      	<br/>
+      	<textarea />
+      	<br/>
+      	<button>submit</button>
+      </div>
+    );
+  }
+}
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,14 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('Render proper text' , () => {
-  //let component;
+describe('App testing' , () => {
+  let component;
 
-  //beforeEach(() => {
-    const component = renderComponent(App);
-  //});
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
 
-  it('shows correct text', () => {
-    expect(component).to.contain('React simple starter');
+  it('shows a comment box', () => {
+    expect(component.find(".comment-box")).to.exist;
   });
 });
